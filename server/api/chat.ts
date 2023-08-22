a9e45546e76563b744d7de95b330fa9ae7d82db7
Pooya Parsa <pooya@pi0.io>
2023-08-22 12:41:59 +0200

    try with new `openai@4` sdk

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,28 +1,21 @@
-import {
-  type ChatCompletionRequestMessage,
-  Configuration,
-  OpenAIApi,
-} from "openai-edge";
+import { CreateChatCompletionRequestMessage } from "openai/resources/chat";
+import OpenAI from "openai";
 import { OpenAIStream } from "ai";
 
 export default defineLazyEventHandler(async () => {
   // Create an OpenAI API client (that's edge friendly!)
   const apiKey = useRuntimeConfig().openaiApiKey;
   if (!apiKey) throw new Error("Missing OpenAI API key");
-  const openai = new OpenAIApi(
-    new Configuration({
-      apiKey,
-    })
-  );
+  const openai = new OpenAI({ apiKey });
 
   return defineEventHandler(async (event) => {
     // Extract the `prompt` from the body of the request
     const { messages } = (await readBody(event)) as {
-      messages: ChatCompletionRequestMessage[];
+      messages: CreateChatCompletionRequestMessage[];
     };
 
     // Ask OpenAI for a streaming chat completion given the prompt
-    const response = await openai.createChatCompletion({
+    const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       stream: true,
       messages: messages.map((message) => ({
